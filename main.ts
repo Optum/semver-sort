129992f62fed5cac456fe0dea645f0c711fa80b0
Chase, Justin M <justin.chase@optum.com>
2022-10-14 12:34:47 -0500

    dont error on epipe

--- main.ts
+++ main.ts
@@ -8,6 +8,16 @@ const versions = lines
   .map((l) => l.trim())
   .filter((l) => l);
 
-for (const { version } of semverSort(...versions)) {
-  console.log(version);
+try {
+  for (const { version } of semverSort(...versions)) {
+    console.log(version);
+  }
+} catch (err) {
+  const { code } = err
+  if (code === "EPIPE") {
+    // ignore, this can happen if stdout is closed by a piped process
+    // such as `semver-sort | head -n 1` which will close stdout after a single line.
+  } else {
+    throw err;
+  }
 }
